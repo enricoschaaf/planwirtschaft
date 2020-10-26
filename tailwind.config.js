23118154108e264d4d4f0872a61326de803210c8
Enrico Schaaf <enricoschaaf98@gmail.com>
2020-10-26 15:07:56 +0100

    Remove unused variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@ module.exports = {
   future: "all",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: [],
-  variants: {
-    variants: {
-      opacity: ({ after }) => after(["disabled"]),
-    },
-  },
+  variants: {},
   plugins: [require("@tailwindcss/ui")],
 }
